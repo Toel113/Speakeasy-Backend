fc6447b219ed09f13bb52a87b62737f39bc51666
Toel113 <toelzx113@gmail.com>
2025-09-09 00:41:19 +0700

    commit json

--- config/config-firebase.js
+++ config/config-firebase.js
@@ -1,5 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require(process.env.FIREBASE_SERVICE_ACCOUNT);
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 let db, auth, bucket;
 
